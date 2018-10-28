e32f8ac93d156f3ca261b17fe4630fb45b819201
patrickyu612101 <39136999+patrickyu612101@users.noreply.github.com>
2018-10-28 15:50:24 -0500

    can't remove circles

--- whole test/static/js/app.js
+++ whole test/static/js/app.js
@@ -1,39 +1,42 @@
 const API_KEY="pk.eyJ1IjoicGF0cmlja3l1NjEyMTAxIiwiYSI6ImNqbjB4czVrcDBwM20zcnBia2l1cDI2OHgifQ.Q-MP65O4i2w_HMphgGb_LA";
 var maptag = d3.select("#map");
+// var layergroup=L.layerGroup([markers]);
+var markers;
 // get data and print it 
 var crashdata=d3.json("/data").then((data)=> {
-console.log(data);
-console.log("ggggggg",Object.values(data)[0].Lat);
-console.log(data[0].Long);
+
 makemap(data);
 });
-
-
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
-var myMap = L.map("map", {
-  center: [38.6270, -90.1994],
-  zoom: 13
-});
-
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
-// actuall data
+var myMap;
+function mapping(){
+    maptag.html("");
+  myMap = L.map("map", {
+    center: [38.6270, -90.1994],
+    zoom: 13
+  });
+  
+  var alllayer=L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);  
+}
+mapping();
 
 // make map
 function makemap(dataset){
   // maptag.html("");
+//  try{
+//     markers.clearLayers();  
+//     }catch(error){ console.log(error)}
+//   maptag.html("");
+
+
   var dataL=Object.keys(dataset).length
 
-var color=""
-  // try{
+  var color=""
+  try{
   for (var i=0;i<dataL;i++){
     if (dataset[i].Severity =="Fatal" ) {
       color = "red";
@@ -44,7 +47,8 @@ var color=""
     else if (dataset[i].Severity =="Property Damage") {
       color = "green";
     }
-    var markers=L.circle([dataset[i].Lat,dataset[i].Long], {
+    // markers = L.markerClusterGroup();
+    markers=L.circle([dataset[i].Lat,dataset[i].Long], {
       fillOpacity: 0.75,
       color: "none",
       fillColor: color,
@@ -53,24 +57,20 @@ var color=""
     }).bindPopup("<h1>Date: " + dataset[i].Date + "</h1> <hr><h1>Weekday: " + dataset[i].Weekday + "</h1> <hr> <h1>Time: " + dataset[i].Time + "</h1><hr><h1>Type: " + dataset[i].Type + "</h1> <hr><h2>Severity: " + dataset[i].Severity + "</h2>").addTo(myMap);
   };
 
-// }catch(error){
-//   console.log(error)
-// }
+}catch(error){
+  console.log(error)
+}
 }
 // End make map
 
 
-// try {
-//   eval('alert("Hello world)');
-// }
-// catch(error) {
-//   console.error(error);
-// }
-// console.log("this is what i need?",data)
-
-// d3.selectAll("#filterbtn").on("click", handleClick);
 function handleClick() {
-  maptag.html("");
+// console.log(markers)
+  // try{
+  //   markers.clearLayers();  
+  //   }catch(error){ console.log(error)}
+  // maptag.html("");
+  markers.clearLayers();
   var crashdata=d3.json("/data").then((data)=> {
   // d3.event.preventDefault();
   var start_date = d3.select("#start_date").property("value");
@@ -79,38 +79,72 @@ function handleClick() {
   var end_time = d3.select("#end_time").property("value");
   var start_week = d3.select("#start_week").property("value");
   var end_week = d3.select("#end_week").property("value");
-  var severity = d3.select("#severity").property("value");
+  var severityuser = d3.select("#severity").property("value");
   var crash_type = d3.select("#crash_type").property("value");
   // var date = d3.select("#datetime").property("value");
-console.log(start_date,end_date,start_time,end_time,start_week,end_week,severity);
-var cleaned=data;
-var dataL=Object.keys(data).length;
-// console.log(Object.values(cleaned)[0].Severity);
-try{
-  // for (var i=0;i<dataL;i++){
-    
-  // }
-  (i,clean) => clean[i].Severity =="Fatal"
-if (data) {
-  console.log("test");
-  cleaned = cleaned.filter(cleaned,(i,value) => cleaned[i].Severity =="Fatal");
-}
+console.log(start_date,end_date,start_time,end_time,start_week,end_week,severityuser);
+
+
+// console.log(result);
+var cleaned=Object.values(data)
+console.log("just=data");
+console.log(cleaned);
+
+try{  
+  var donedata = cleaned.filter(function(cleaned) {
+    return cleaned.Severity=="Fatal";
+  });
+  // donedata = cleaned.filter(function(cleaned) {
+  //   return cleaned.Type==crash_type;
+  // });
+
 }catch(error){
   console.log(error);
 };  
+console.log("cleaned");
+console.log(donedata);
+// console.log(done);
 
-console.log(cleaned);
-makemap(cleaned);
+makemap2(donedata);
 
 });
-//   d3.event.preventDefault();
-
-  // var date = d3.select("#datetime").property("value");
-//   let filteredData = tableData;
-//   if (date) {
-//     filteredData = filteredData.filter(row => row.datetime === date);
-//   }
-//   buildTable(filteredData);
 }
 
+// make 2
+function makemap2(dataset){
+////////////////
 
+mapping();
+// /////////////
+
+
+  // maptag.html("");
+  var dataL=dataset.length
+  console.log(dataL)
+  console.log(dataset[0].Severity)
+
+var color=""
+  try{
+  for (var i=0;i<dataL;i++){
+    if (dataset[i].Severity =="Fatal" ) {
+      color = "red";
+    }
+    else if (dataset[i].Severity =="Personl Injury") {
+      color = "orange";
+    }
+    else if (dataset[i].Severity =="Property Damage") {
+      color = "green";
+    }
+    markers=L.circle([dataset[i].Lat,dataset[i].Long], {
+      fillOpacity: 0.75,
+      color: "none",
+      fillColor: color,
+      // Adjust radius
+      radius: 500
+    }).bindPopup("<h1>Date: " + dataset[i].Date + "</h1> <hr><h1>Weekday: " + dataset[i].Weekday + "</h1> <hr> <h1>Time: " + dataset[i].Time + "</h1><hr><h1>Type: " + dataset[i].Type + "</h1> <hr><h2>Severity: " + dataset[i].Severity + "</h2>").addTo(myMap);
+  };
+
+}catch(error){
+  console.log(error)
+}
+}
